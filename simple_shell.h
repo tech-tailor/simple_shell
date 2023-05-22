c030d639d8d7823d3ca69dddcb70c4d8af8fb444
tech-tailor <ogundareakinniyi8@gmail.com>
2023-05-22 10:33:23 +0000

    header for the simple shell

---
+++ simple_shell.h
@@ -0,0 +1,18 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+ void read_from_terminal(char *comd, char **par);
+ void display_prompt();
+ int main(int argc, char **argv, char **envp);
+
+
+
+
+
+#endif
